3cc701708540b22a284d105eb2f22e5460d8bc44
Stefan Lindörfer <stefan.lindoerfer@me.com>
2020-08-11 23:23:50 +0200

    Kopierfehler ausgebessert. Keine Inhaltlichen Änderungen

--- main.cpp
+++ main.cpp
@@ -48,17 +48,17 @@ void setup() {
 
   // following if statement shouldn't be neccessary, usefull for debugging purposes
   if (!status) {
-      Serial.println(Could not find a valid BME280 sensor, check wiring, address, sensor ID!);
-      Serial.print(SensorID was: 0x); Serial.println(bme.sensorID(),16);
-      Serial.print( ID of 0xFF probably means a bad address, a BMP 180 or BMP 085n);
-      Serial.print( ID of 0x56-0x58 represents a BMP 280,n);
-      Serial.print( ID of 0x60 represents a BME 280.n);
-      Serial.print( ID of 0x61 represents a BME 680.n);
+      Serial.println("Could not find a valid BME280 sensor, check wiring, address, sensor ID!");
+      Serial.print("SensorID was: 0x); Serial.println(bme.sensorID(),16);
+      Serial.print(" ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
+      Serial.print(" ID of 0x56-0x58 represents a BMP 280.\n");
+      Serial.print(" ID of 0x60 represents a BME 280.n");
+      Serial.print(" ID of 0x61 represents a BME 680.n");
       while (1) delay(10);
   }
 
   // write message if everything worked fine
-  Serial.println(F(System set up!));
+  Serial.println(F("System set up!"));
 }
 
 void loop() {
