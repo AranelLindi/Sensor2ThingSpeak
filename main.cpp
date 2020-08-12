bbbc86b1c9936dbb69031feb8cc39c72747b9219
Stefan Lindörfer <stefan.lindoerfer@me.com>
2020-08-12 10:32:09 +0200

    Weitere Kopierfehler ausgebessert und Kommentare auf Deutsch verfasst sowie detaillierter Kommentiert.

--- main.cpp
+++ main.cpp
@@ -11,8 +11,8 @@
 // ThingSpeak
 #include "ThingSpeak.h"
 
-// own code
-#include "secrets.h" // important: in .gitignore! Contains wlan ssid & password!
+// eigener Code
+#include "secrets.h" // wichtig: in .gitignore! Enthält SSID, Wlan-Passwort und ThingSpeak Login!
 
 
 // ThingSpeak
@@ -23,12 +23,12 @@ const char* myWriteAPIKey = SECRET_WRITE_APIKEY; // defined in secrets.h
 const char* ssid = SECRET_SSID; // defined in secrets.h
 const char* pass = SECRET_PASS; // defined in secrets.h
 
-// periodic status reports
-const uint16_t interval = 60; // Update statistics and measures every 60 seconds
+// Interval in Sekunden
+const uint16_t interval = 60; // Wartezeit nach einer Schleifeniteration
 
 WiFiClient client; // web communication
 
-// instance of bme280 sensor
+// Instanz des bme280 Sensors
 Adafruit_BME280 bme; // I2C
 
 
@@ -36,33 +36,33 @@ void setup() {
   Serial.begin(115200);
   while(!Serial); // time to get serial running
   
-  //WiFi.init(&Serial); // decomment for debugging (posts Wlan Messages)
+  //WiFi.init(&Serial); // entkommentieren für debugging (postet Wlan Rohtext)
 
-  // Connect to wlan...
-  WiFi.mode(WIFI_STA);
-  // ... and internet
+  WiFi.mode(WIFI_STA); // Legt den Zustand von ESP im Wlan fest (STA = station = client)
+  
+  // Verbindung mit Thingspeak aufbauen (obwohl Wlan Verbindung noch nicht steht)
   ThingSpeak.begin(client);
 
-  // initialize BME
+  // BME initialisieren (0x76 = Adresse)
   uint32_t status = bme.begin(0x76);
 
-  // following if statement shouldn't be neccessary, usefull for debugging purposes
-  /*if (!status) {
+  // Schreibt Nachricht in seriellen Anschluss, wenn Verbindungsprobleme mit BME auftreten
+  if (!status) {
       Serial.println("Could not find a valid BME280 sensor, check wiring, address, sensor ID!");
-      Serial.print("SensorID was: 0x); Serial.println(bme.sensorID(),16");
+      Serial.print("SensorID was: 0x"); Serial.println(bme.sensorID(),16);
       Serial.print(" ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
       Serial.print(" ID of 0x56-0x58 represents a BMP 280.\n");
       Serial.print(" ID of 0x60 represents a BME 280.\n");
       Serial.print(" ID of 0x61 represents a BME 680.\n");
       while (1) delay(10);
-  }*/
+  }
 
-  // write message if everything worked fine
-  Serial.println("System set up!");
+  // Nachricht ausgeben, wenn bis hierhin alles funktioniert hat
+  Serial.println("System works fine! Start measuring...");
 }
 
 void loop() {
-  // Connect or reconnect to WiFi
+  // Verbinden oder Wiederverbinden mit WiFi
   if (WiFi.status() != WL_CONNECTED) {
     Serial.print("Attempting to connect to SSID: ");
     Serial.println(SECRET_SSID);
@@ -78,22 +78,24 @@ void loop() {
     Serial.println(WiFi.localIP());
   }
 
+  // Eventuell mal nice to have:
   // Measure Signal Strength (RSSI) of Wi-Fi connection
   //int64_t rssi = WiFi.RSSI();
 
-  // write values to Field 1-3 (Temperature, Humidity, Pressure) of a ThingSpeak Channel
+  // setzt die neuen Messwerte für die einzelnen Felder entsprechend... (Temperatur, Humidity, Pressure)
   ThingSpeak.setField(1, bme.readTemperature());
   ThingSpeak.setField(2, bme.readHumidity());
   ThingSpeak.setField(3, bme.readPressure());
 
-  // send values to ThingSpeak website
+  // ... und sendet sie zur ThingSpeak Website
   int32_t httpCode = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
-    
+  
+  // prüfen ob Fehler aufgetreten sind und entsprechend melden
   if (httpCode == 200)
     Serial.println("Channel write successful.");
   else
     Serial.println("Problem writing to channel. HTTP error code"  + String(httpCode));
 
-  // Wait [interval]-seconds to update the channel again
+  // Intervall-Zeit abwarten und mit nächster Iteration fortfahren
   delay(interval * 1000); 
 }
